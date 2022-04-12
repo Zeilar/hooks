8d52f691c2176128cd7af387d18d9ed353dff9f5
Philip <philip_angelin@hotmail.com>
2022-04-12 08:57:50 +0200

    docs(useIsMounted): comment

--- src/useIsMounted.ts
+++ src/useIsMounted.ts
@@ -2,6 +2,10 @@ import { useCallback, useRef } from "react";
 import { useOnUnmount } from "./useOnUnmount";
 import { useOnMount } from "./useOnMount";
 
+/**
+ * Useful for running code only after the first render.
+ * @example const isMounted() = useIsMounted();
+ */
 export function useIsMounted() {
 	const isMounted = useRef(false);
 	useOnMount(() => {
