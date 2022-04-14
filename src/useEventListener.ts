1cf505270fe32b979ffa1e4474e4a3df2dd0e50f
Philip <philip_angelin@hotmail.com>
2022-04-14 15:51:28 +0200

    refactor(useEventListener): return element instead

--- src/useEventListener.ts
+++ src/useEventListener.ts
@@ -4,18 +4,28 @@ import { useEffect, useRef } from "react";
  * Useful for avoiding a lot of boilerplate code.
  * @example useEventListener(element, "click", clickHandler);
  */
-export function useEventListener(element: Element, event: keyof GlobalEventHandlersEventMap, callback: () => any) {
+export function useEventListener<T extends HTMLElement = HTMLDivElement>(
+	event: keyof GlobalEventHandlersEventMap,
+	callback: () => any
+) {
 	const callbackRef = useRef(callback);
+	const elementRef = useRef<T>(null);
 
 	useEffect(() => {
 		callbackRef.current = callback;
 	}, [callback]);
 
 	useEffect(() => {
+		const element = elementRef.current;
+		if (!element) {
+			return;
+		}
 		const callback = callbackRef.current;
 		element.addEventListener(event, callback);
 		return () => {
 			element.removeEventListener(event, callback);
 		};
-	}, [event, element]);
+	}, [event, elementRef]);
+
+	return elementRef;
 }
