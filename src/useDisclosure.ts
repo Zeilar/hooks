3ca89d45c1726c2a8f62ba6aaba0ffe584438044
Philip <philip_angelin@hotmail.com>
2022-04-16 01:22:58 +0200

    feat(useDisclosure): give initialState arg

--- src/useDisclosure.ts
+++ src/useDisclosure.ts
@@ -11,8 +11,8 @@ export interface UseDisclosureReturn {
  * Useful for abstracting disclosure logic, e.g the visibility state of a modal.
  * @example const { isOpen, open, close, toggle } = useDisclosure();
  */
-export function useDisclosure() {
-	const [isOpen, setIsOpen] = useState(false);
+export function useDisclosure(initialState: boolean = false) {
+	const [isOpen, setIsOpen] = useState(initialState);
 
 	function open() {
 		setIsOpen(true);
