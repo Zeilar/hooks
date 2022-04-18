b2f577260a3843d9b58f00189f9c4974e9986cc9
Philip <philip_angelin@hotmail.com>
2022-04-18 13:40:04 +0200

    fix(useBodyScroll): compute initial state

--- src/useBodyScroll.ts
+++ src/useBodyScroll.ts
@@ -1,13 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import { useOnUnmount } from "./useOnUnmount";
 
+export function isBodyScrollable() {
+	const bodyOverflowStyle = getComputedStyle(document.body).overflowY;
+	return bodyOverflowStyle === "auto" || bodyOverflowStyle === "visible";
+}
+
 /**
- * Useful for removing page scrolling, e.g when a modal is open.
+ * Useful for removing page scrolling, e.g when a modal is open. Keep in mind this only checks overflowY.
  * @example const [hasScroll, setHasScroll] = useTheme("dark");
  */
 export function useBodyScroll() {
 	const originalOverflow = useRef(document.body.style.overflowY);
-	const [hasScroll, setHasScroll] = useState(originalOverflow.current);
+	const [hasScroll, setHasScroll] = useState(isBodyScrollable());
 
 	useEffect(() => {
 		document.body.style.overflowY = hasScroll ? originalOverflow.current : "hidden";
