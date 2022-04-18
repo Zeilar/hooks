8e70c7a239cfbca13020a3c354aca78f39bd0b0e
Philip <philip_angelin@hotmail.com>
2022-04-18 22:17:59 +0200

    fix(useBodyScroll): overflowY is buggy

--- src/useBodyScroll.ts
+++ src/useBodyScroll.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useOnUnmount } from "./useOnUnmount";
 
 export function isBodyScrollable() {
-	switch (getComputedStyle(document.body).overflowY) {
+	switch (getComputedStyle(document.body).overflow) {
 		case "auto":
 		case "visible":
 			return true;
@@ -14,19 +14,19 @@ export function isBodyScrollable() {
 }
 
 /**
- * Useful for removing page scrolling, e.g when a modal is open. Keep in mind this only checks overflowY.
- * @example const [hasScroll, setHasScroll] = useTheme("dark");
+ * Useful for removing page scrolling, e.g when a modal is open. Keep in mind this only checks overflow.
+ * @example const [hasScroll, setHasScroll] = useBodyScroll();
  */
 export function useBodyScroll() {
-	const originalOverflow = useRef(document.body.style.overflowY);
+	const originalOverflow = useRef(document.body.style.overflow);
 	const [hasScroll, setHasScroll] = useState(isBodyScrollable());
 
 	useEffect(() => {
-		document.body.style.overflowY = hasScroll ? originalOverflow.current : "hidden";
+		document.body.style.overflow = hasScroll ? originalOverflow.current : "hidden";
 	}, [hasScroll]);
 
 	useOnUnmount(() => {
-		document.body.style.overflowY = originalOverflow.current;
+		document.body.style.overflow = originalOverflow.current;
 	});
 
 	return [hasScroll, setHasScroll] as const;
